97aad21584b27bfdc4dca91d59b3d4d567f3f77c
KangShiang <ykshiang@gmail.com>
2016-02-14 23:45:43 -0800

    Initial Commit

---
+++ Vanfiider/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Vanfiider
+//
+//  Created by Kang Shiang Yap on 2016-02-14.
+//  Copyright © 2016 Fiidup. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
